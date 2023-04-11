5fdbf939079cef6cc8e4907ec01d55c2e03b6ded
Juan Jose <juanjose.sierra@utp.edu.co>
2023-04-10 20:37:09 -0500

    Ya se pueden eliminar tareas, sigue hacer un contexto para las tareas para que cuando se borre se refleje en todos lados

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,11 +1,24 @@
+import { deleteTaskRequest } from "../api/task.api"
+
 function TaskCard({task}){
+
+    const handleDelete = async(id) => {
+        try {
+            const response = await deleteTaskRequest(id)
+            console.log(response)
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
+
     return(
         <div>
             <h2>{task.title}</h2>
             <p>{task.description}</p>
             <span>{task.done == 1 ? "✔️" : "❌"}</span>
             <span>{task.createAt}</span>
-            <button>
+            <button onClick={() => handleDelete(task.id)}>
                 Delete
             </button>
             <button>
--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -14,17 +14,19 @@ function TaskPage({userId}){
         loadTasks()
     }, [])
 
+    function renderMain(){
+        if(tasks.length === 0) return <h1>No tasks yet</h1>
+
+        return tasks.map(task => (<TaskCard task={task} key={task.id} />))
+    }
+
     return(
         <div>
             <h1>
                 Tasks
             </h1>
 
-            {
-                tasks.map(task => (
-                    <TaskCard task={task} key={task.id} />
-                ))
-            }
+            {renderMain()}
 
         </div>
     )
