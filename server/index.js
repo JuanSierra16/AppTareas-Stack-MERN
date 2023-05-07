2bc97aa19ce718033c5e07a7f42c0ee77caf9a68
Juan Jose <juanjose.sierra@utp.edu.co>
2023-05-06 20:08:22 -0500

    Se le hizo build al proyecto, ya solo es necesario hacer npm start desde el servidor sin necesidad de ejecutar el cliente

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import {dirname, join} from "path";
+import { fileURLToPath } from "url";
 import {PORT} from "./config.js";
 
 import indexRoutes from './routes/index.routes.js';
@@ -7,6 +9,7 @@ import taskRoutes from './routes/tasks.routes.js';
 import usersRoutes from './routes/users.routes.js';
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(cors());//Aqui se le puede decir que servidor se puede conectar, en este caso esta para que cualquiera pueda, pero se puede modificar
 app.use(express.json());
@@ -14,6 +17,8 @@ app.use(express.json());
 app.use(indexRoutes);
 app.use(taskRoutes);
 app.use(usersRoutes);
+
+app.use(express.static(join(__dirname, '../client/dist')))
 
 app.listen(PORT);
 console.log(`Server is running on port ${PORT}`);
