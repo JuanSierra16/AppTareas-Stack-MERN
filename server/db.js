76546ce5ff6e1c89ee8d46b9318eacb600405ab5
Juan Jose <juanjose.sierra@utp.edu.co>
2023-05-06 20:42:45 -0500

    Ya todo listo para subirlo a github y a produccion

--- server/db.js
+++ server/db.js
@@ -1,9 +1,10 @@
 import {createPool} from "mysql2/promise";
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_PORT } from "./config.js";
 
 export const pool = createPool({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '123456789',
-    database: 'MiSistemaAT'
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME
 });
